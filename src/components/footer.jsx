a227542e39252b2f5b3acc272752ccc1312a9b15
Jake Deng <deng.jake@gmail.com>
2024-01-24 21:12:20 -0500

    1.1.1 - fix error loop bug

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,9 @@ import useAuthCheckQuery from '../queries/authCheckQuery.jsx';
 function Footer() {
     const authCheckQ = useAuthCheckQuery(true);
 
-    if (authCheckQ.data.isAuth) {
+    if (authCheckQ.isError) {
+        return <></>;
+    } else if (authCheckQ.data.isAuth) {
         return (
             <div className='font-semibold mb-5 px-3.5 xxs:px-5 text-center '>
                 <a href='https://github.com' className='text-sm text-blue-300 hover:underline' target="_blank" rel="noopener noreferrer">
